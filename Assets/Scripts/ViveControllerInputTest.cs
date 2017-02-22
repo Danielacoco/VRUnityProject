f1f9cf780a5fb8411068d7465607553b403928c9
Danielacoco <danielacoor6@gmail.com>
2017-02-21 19:18:41 -0800

    Initial commit, interactions and telerasports working

---
+++ Assets/Scripts/ViveControllerInputTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveControllerInputTest : MonoBehaviour {
+
+    /*Reference to object being tracked*/
+    private SteamVR_TrackedObject trackedObj;
+
+    /*Device property fo easy acces to contoller's input*/
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
